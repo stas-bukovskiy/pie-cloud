8ef0962d91e77d0ab811d10cf9b10be7d38d6a19
Stas <stbukovskiy12@gmail.com>
2023-01-19 15:43:40 +0200

    Initial commit

---
+++ src/main/java/com/piecloud/PieCloudApplication.java
@@ -0,0 +1,13 @@
+package com.piecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PieCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piecloud/PieCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
