ed7c9ddc135018ac8fa293df88086200e9c4eaf1
Stas <stbukovskiy12@gmail.com>
2023-02-18 13:31:45 +0200

    Add validation for AuthenticationRequest

--- src/main/java/com/piecloud/user/AuthenticationRequest.java
+++ src/main/java/com/piecloud/user/AuthenticationRequest.java
@@ -1,14 +1,17 @@
 package com.piecloud.user;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter @Setter
 public class AuthenticationRequest {
 
+    @NotBlank(message = "username is mandatory")
     private String username;
+    @NotBlank(message = "password is mandatory")
     private String password;
 
 }
