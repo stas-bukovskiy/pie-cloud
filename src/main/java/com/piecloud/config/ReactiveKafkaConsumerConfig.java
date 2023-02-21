f957b5d63d7ea5dd1f0e66dfdb441c69bf8c4f76
Stas <stbukovskiy12@gmail.com>
2023-02-21 19:46:50 +0200

    Create configs for producer and consumer

---
+++ src/main/java/com/piecloud/config/ReactiveKafkaConsumerConfig.java
@@ -0,0 +1,28 @@
+package com.piecloud.config;
+
+import com.piecloud.order.OrderDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.Collections;
+
+@Configuration
+public class ReactiveKafkaConsumerConfig {
+    @Bean
+    public ReceiverOptions<String, OrderDto> kafkaReceiverOptions(@Value(value = "${ORDER_DTO_TOPIC}") String topic,
+                                                                  KafkaProperties kafkaProperties) {
+        ReceiverOptions<String, OrderDto> basicReceiverOptions =
+                ReceiverOptions.create(kafkaProperties.buildConsumerProperties());
+        return basicReceiverOptions.subscription(Collections.singletonList(topic));
+    }
+
+    @Bean
+    public ReactiveKafkaConsumerTemplate<String, OrderDto> reactiveKafkaConsumerTemplate(
+            ReceiverOptions<String, OrderDto> kafkaReceiverOptions) {
+        return new ReactiveKafkaConsumerTemplate<>(kafkaReceiverOptions);
+    }
+}
---
+++ src/main/java/com/piecloud/config/ReactiveKafkaProducerConfig.java
@@ -0,0 +1,21 @@
+package com.piecloud.config;
+
+import com.piecloud.order.OrderDto;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
+import reactor.kafka.sender.SenderOptions;
+
+import java.util.Map;
+
+@Configuration
+public class ReactiveKafkaProducerConfig {
+    @Bean
+    public ReactiveKafkaProducerTemplate<String, OrderDto> reactiveKafkaProducerTemplate(
+            KafkaProperties properties) {
+        Map<String, Object> props = properties.buildProducerProperties();
+        return new ReactiveKafkaProducerTemplate<>(SenderOptions.create(props));
+    }
+
+}
