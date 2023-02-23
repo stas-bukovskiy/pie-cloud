1f5fee99fb657e9f45d332ee6b0e70b422d601d7
Stas <stbukovskiy12@gmail.com>
2023-02-23 18:33:31 +0200

    Create test profile

--- src/test/java/com/piecloud/PieCloudApplicationTests.java
+++ src/test/java/com/piecloud/PieCloudApplicationTests.java
@@ -1,9 +1,13 @@
 package com.piecloud;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootTest
+@Profile("test")
 class PieCloudApplicationTests {
 
     @Test
