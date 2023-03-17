5b0bd30db3eb4b0e52f8786bcfbfd947cff3c15c
Stas <stbukovskiy12@gmail.com>
2023-03-17 12:19:02 +0200

    Create OrderServiceTest, OrderControllerTest,

---
+++ src/test/java/com/piecloud/order/OrderControllerTest.java
@@ -0,0 +1,229 @@
+package com.piecloud.order;
+
+import com.piecloud.RandomStringUtils;
+import com.piecloud.addition.Addition;
+import com.piecloud.addition.AdditionRepository;
+import com.piecloud.addition.group.AdditionGroup;
+import com.piecloud.addition.group.AdditionGroupRepository;
+import com.piecloud.ingredient.Ingredient;
+import com.piecloud.ingredient.IngredientRepository;
+import com.piecloud.ingredient.group.IngredientGroup;
+import com.piecloud.ingredient.group.IngredientGroupRepository;
+import com.piecloud.pie.Pie;
+import com.piecloud.pie.PieRepository;
+import com.piecloud.security.jwt.JwtTokenProvider;
+import com.piecloud.user.User;
+import com.piecloud.user.UserRepository;
+import com.piecloud.user.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.piecloud.addition.RandomAdditionUtil.randomAddition;
+import static com.piecloud.addition.group.RandomAdditionGroupUtil.randomAdditionGroup;
+import static com.piecloud.ingredient.RandomIngredientUtil.randomIngredient;
+import static com.piecloud.ingredient.group.RandomIngredientGroupUtil.randomIngredientGroup;
+import static com.piecloud.order.RandomOrderUtil.*;
+import static com.piecloud.pie.PieUtil.randomPie;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DirtiesContext
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class OrderControllerTest {
+
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "1234";
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private AdditionRepository additionRepository;
+    @Autowired
+    private AdditionGroupRepository additionGroupRepository;
+    @Autowired
+    private PieRepository pieRepository;
+    @Autowired
+    private IngredientRepository ingredientRepository;
+    @Autowired
+    private IngredientGroupRepository ingredientGroupRepository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private OrderConverter converter;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private String token;
+    private Addition addition;
+    private Pie pie;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll()
+                .then(userRepository.save(new User(
+                        null,
+                        USERNAME,
+                        passwordEncoder.encode(PASSWORD),
+                        RandomStringUtils.randomEmail(),
+                        true,
+                        List.of("ADMIN", "COOK")
+                ))).block();
+        token = tokenProvider.createToken(new UsernamePasswordAuthenticationToken(USERNAME, PASSWORD));
+
+        orderRepository.deleteAll().block();
+        IngredientGroup group = ingredientGroupRepository.deleteAll()
+                .then(ingredientGroupRepository.save(randomIngredientGroup()))
+                .block();
+
+        List<Ingredient> ingredients = ingredientRepository.deleteAll()
+                .thenMany(ingredientRepository.saveAll(List.of(
+                        randomIngredient(group),
+                        randomIngredient(group),
+                        randomIngredient(group)
+                ))).collectList().block();
+        assert ingredients != null;
+        pie = pieRepository.save(randomPie(ingredients)).block();
+
+        AdditionGroup additionGroup = additionGroupRepository.save(randomAdditionGroup()).block();
+        addition = additionRepository.save(randomAddition(additionGroup)).block();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        orderRepository.deleteAll().block();
+        userRepository.deleteAll().block();
+        ingredientGroupRepository.deleteAll().block();
+        ingredientRepository.deleteAll().block();
+        pieRepository.deleteAll().block();
+        additionGroupRepository.deleteAll().block();
+        additionRepository.deleteAll().block();
+    }
+
+    @Test
+    @WithMockUser(username = USERNAME, password = PASSWORD)
+    void getAllOrders() {
+        List<Order> orders = List.of(
+                randomOrder(addition, pie, pie),
+                randomOrder(addition, pie, pie),
+                randomOrder(addition, pie, pie)
+        );
+        User user = userService.getCurrentUser().block();
+        assertNotNull(user);
+        orders.forEach(order -> order.setUserId(user.getId()));
+        orders = orderRepository.saveAll(orders).collectList().block();
+        assertNotNull(orders);
+        assertEquals(3, orders.size());
+
+        webTestClient
+                .get()
+                .uri("/api/order/")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(OrderDto.class)
+                .hasSize(orders.size());
+    }
+
+    @Test
+    @WithMockUser(username = USERNAME, password = PASSWORD)
+    void postOrder() {
+        Pie pieWithIngredients = new Pie();
+        pieWithIngredients.setIngredients(pie.getIngredients());
+        Order order = randomOrder(addition, pie, pieWithIngredients);
+        OrderDto orderDtoToPost = converter.convertDocumentToDto(order);
+        webTestClient
+                .post()
+                .uri("/api/order/")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .bodyValue(orderDtoToPost)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(OrderDto.class)
+                .value(postedOrderDto -> {
+                    assertNotNull(postedOrderDto.getId());
+                    assertEquals(OrderStatus.IN_LINE, postedOrderDto.getStatus());
+                    assertNotNull(postedOrderDto.getCreatedDate());
+                    assertEquals(countPrice(converter.convertDtoToDocument(postedOrderDto)), postedOrderDto.getPrice());
+                });
+    }
+
+    @Test
+    @WithMockUser(username = USERNAME, password = PASSWORD, roles = "ADMIN")
+    void postOrderWithInvalidOrderLines() {
+        OrderDto orderDtoToPost = randomOrderDto();
+        webTestClient
+                .post()
+                .uri("/api/order/")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .bodyValue(orderDtoToPost)
+                .exchange()
+                .expectStatus().isEqualTo(400);
+    }
+
+    @Test
+    @WithMockUser(username = USERNAME, password = PASSWORD, roles = {"ADMIN"})
+    void changeStatus() {
+        Order order = randomOrder(addition, pie, pie);
+        order.setId(null);
+        OrderStatus status = OrderStatus.COMPLETED;
+        order = orderRepository.save(order).block();
+        assertNotNull(order);
+
+        webTestClient.patch()
+                .uri("/api/order/{id}/status", order.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(Map.of("status", status.name()))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(OrderDto.class)
+                .value(orderDto -> assertEquals(OrderStatus.COMPLETED, orderDto.getStatus()));
+    }
+
+    @Test
+    @WithMockUser(username = USERNAME, password = PASSWORD, roles = {"ADMIN"})
+    void changeInvalidStatus_shouldReturn400() {
+        Order order = randomOrder(addition, pie, pie);
+        orderRepository.save(order).block();
+
+        webTestClient.patch()
+                .uri("/api/order/{id}/status", order.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(Map.of("status", "invalid"))
+                .exchange()
+                .expectStatus().isEqualTo(400);
+    }
+
+    @Test
+    @WithMockUser(username = USERNAME, password = PASSWORD, roles = {"ADMIN"})
+    void changeNullStatus_shouldReturn400() {
+        Order order = randomOrder(addition, pie, pie);
+        orderRepository.save(order).block();
+
+        webTestClient.patch()
+                .uri("/api/order/{id}/status", order.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(400);
+    }
+}
---
+++ src/test/java/com/piecloud/order/OrderServiceTest.java
@@ -0,0 +1,131 @@
+package com.piecloud.order;
+
+import com.piecloud.order.line.OrderLine;
+import com.piecloud.order.line.OrderLineService;
+import com.piecloud.user.User;
+import com.piecloud.user.UserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Sort;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.piecloud.order.RandomOrderUtil.randomOrder;
+import static com.piecloud.user.UserUtils.randomUser;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.data.domain.Sort.Direction.ASC;
+
+
+@SpringBootTest
+class OrderServiceTest {
+
+    @Autowired
+    private OrderService service;
+    @Autowired
+    private OrderConverter converter;
+    @MockBean
+    private OrderRepository repository;
+    @MockBean
+    private OrderLineService orderLineService;
+    @MockBean
+    private UserService userService;
+
+
+    @Test
+    void getOrders() {
+        User user = randomUser();
+        List<Order> orders = new ArrayList<>(List.of(
+                randomOrder(),
+                randomOrder(),
+                randomOrder()
+        ));
+
+        Mockito.when(userService.getCurrentUser()).thenReturn(Mono.just(user));
+        Mockito.when(repository.findAllByUserId(user.getId(), Sort.by(ASC, "createdDate")))
+                .thenReturn(Flux.fromIterable(orders));
+
+        orders.sort(Comparator.comparing(Order::getCreatedDate));
+        Flux<OrderDto> result = service.getOrders("createdDate,asc");
+        StepVerifier.create(result)
+                .expectNext(converter.convertDocumentToDto(orders.get(0)))
+                .expectNext(converter.convertDocumentToDto(orders.get(1)))
+                .expectNext(converter.convertDocumentToDto(orders.get(2)))
+                .verifyComplete();
+    }
+
+    @Test
+    void cre1ateOrder() {
+        User user = randomUser();
+        Order savedOrder = randomOrder();
+        OrderDto orderDtoToCreate = converter.convertDocumentToDto(savedOrder);
+        OrderLine orderLineStub = savedOrder.getOrderLines().toArray(new OrderLine[0])[0];
+        savedOrder.setOrderLines(Stream.of(orderLineStub).collect(Collectors.toSet()));
+
+        Mockito.when(orderLineService.createOrderLine(Mockito.any()))
+                .thenReturn(Mono.just(orderLineStub));
+        Mockito.when(userService.getCurrentUser()).thenReturn(Mono.just(user));
+        savedOrder.setUserId(user.getId());
+        Mockito.when(repository.save(new Order(null,
+                        null,
+                        OrderStatus.IN_LINE,
+                        null,
+                        savedOrder.getOrderLines(),
+                        user.getId())))
+                .thenReturn(Mono.just(savedOrder));
+
+        Mono<OrderDto> result = service.createOrder(Mono.just(orderDtoToCreate));
+
+        StepVerifier.create(result)
+                .consumeNextWith(created -> assertEquals(converter.convertDocumentToDto(savedOrder), created))
+                .verifyComplete();
+    }
+
+    @Test
+    void changeStatus() {
+        User user = randomUser();
+        Order order = randomOrder();
+        order.setUserId(user.getId());
+        Mockito.when(userService.getCurrentUser()).thenReturn(Mono.just(user));
+        Mockito.when(repository.findByIdAndUserId(order.getId(), order.getUserId())).thenReturn(Mono.just(order));
+        order.setStatus(OrderStatus.COMPLETED);
+        Mockito.when(repository.save(order)).thenReturn(Mono.just(order));
+        order.setStatus(OrderStatus.IN_LINE);
+
+        Mono<OrderDto> result = service.changeStatus(order.getId(), Mono.just("COMPLETED"));
+
+        StepVerifier.create(result)
+                .consumeNextWith(changedOrder -> assertEquals(OrderStatus.COMPLETED, changedOrder.getStatus()))
+                .verifyComplete();
+    }
+
+    @Test
+    void getUncompletedOrders() {
+        List<Order> orders = new ArrayList<>(List.of(
+                randomOrder(),
+                randomOrder(),
+                randomOrder()
+        ));
+        orders.get(0).setStatus(OrderStatus.COMPLETED);
+        Mockito.when(repository.findAllByStatusNotOrderByStatusAscCreatedDateAsc(OrderStatus.COMPLETED))
+                .thenReturn(Flux.fromIterable(orders.stream()
+                        .filter(order -> !order.getStatus().equals(OrderStatus.COMPLETED))
+                        .toList()));
+
+        orders.sort(Comparator.comparing(Order::getCreatedDate));
+        Flux<OrderDto> result = service.getUncompletedOrders();
+        StepVerifier.create(result)
+                .expectNext(converter.convertDocumentToDto(orders.get(1)))
+                .expectNext(converter.convertDocumentToDto(orders.get(2)))
+                .verifyComplete();
+    }
+}
